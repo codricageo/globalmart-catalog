b625f270618c82d2eb98748defdaff083a78fd8e
codricageo <79510335+codricageo@users.noreply.github.com>
2025-12-09 15:40:28 +0200

    Update Navbar component - latest modifications

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark py-3 sticky-top">
             <div className="container">
-                <NavLink className="navbar-brand fw-bold fs-4 px-2 text-white" to="/"> testing </NavLink>
+                <NavLink className="navbar-brand fw-bold fs-4 px-2 text-white" to="/"> EcomerceWebsite JS </NavLink>
                 <button className="navbar-toggler mx-2" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -21,10 +21,10 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav m-auto my-2 text-center">
                         <li className="nav-item">
-                            <NavLink className="nav-link text-white" to="/">🏠 Home</NavLink>
+                            <NavLink className="nav-link text-white" to="/">🏠 Homes</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink className="nav-link text-white" to="/product">📦 Products</NavLink>
+                            <NavLink className="nav-link text-white" to="/product">📦 Products Test</NavLink>
                         </li>
                         <li className="nav-item">
                             <NavLink className="nav-link text-white" to="/about">ℹ️ About This project</NavLink>
